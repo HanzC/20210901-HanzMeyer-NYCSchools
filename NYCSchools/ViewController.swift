cacb9aa5facca4b7f71320296650373ba7de14c6
Hanz Meyer <hanzcmeyer@gmail.com>
2021-09-01 16:07:22 -0400

    first commit

---
+++ NYCSchools/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NYCSchools
+//
+//  Created by Hanz Meyer on 9/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
